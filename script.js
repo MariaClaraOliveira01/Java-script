1e691bbc6f0079afe293493e2b849570724f4b5a
MariaClaraOliveira01 <165787108+MariaClaraOliveira01@users.noreply.github.com>
2024-10-23 07:39:28 -0300

    OI

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function mudarCor(){
-    const text= documento.getElementeByld('titulo');
+    const text= documento.getElementeById('titulo');
     titulo.Style.color= titulo.style.color=== 'red' ?  'blue' :  'red';
 }
